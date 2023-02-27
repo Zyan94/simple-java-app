ddd0ad3d7db62e199fc8895e11ce129759e4ef50
Zyan Wong <zyanwong94@gmail.com>
2023-02-27 08:34:15 +0000

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -14,6 +14,7 @@ public class App {
         System.out.println("Jenkins is awesome");
         System.out.println("This training is so exciting!");
         System.out.println("Exercise 11, page 30");
+        System.out.println("Set up GitHub WebHook");
     }
 
     public String getMessage() {
